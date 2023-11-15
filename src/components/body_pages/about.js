d955baf7adf0d577df528404155cf4c9679e853a
watchis <warren.atchison98@gmail.com>
2023-11-14 20:25:11 -0500

    adding about me content

--- src/components/body_pages/about.js
+++ src/components/body_pages/about.js
@@ -27,18 +27,43 @@ const About = () => {
           display: "flex",
           flexDirection: "column",
           maxWidth: "60%",
+
+          justifyContent: "center",
         }}
       >
-        <PageTitle style={{ marginTop: "64px", textAlign: "left" }}>
+        <PageTitle style={{ textAlign: "left" }}>
           <span style={{ color: ThemeColors.Orange }}>About</span> Me
         </PageTitle>
         <AboutText>
-          This is a long story of the warren in which the warren is known as the
-          "Warren".
+          Hi! I've been a professional software engineer since January 2020 and
+          love to challenge myself to constantly learn new languages, skills,
+          and frameworks.
         </AboutText>
-        <AboutText>Pickles</AboutText>
-        <AboutText>Pickles</AboutText>
-        <AboutText>Pickles</AboutText>
+        <AboutText>
+          I pride myself on my ability to adapt to new teams, languages, and
+          frameworks quickly. I am highly self-motivated and get most of my
+          enjoyment at work meeting my own internal goals while advancing
+          project milestones. I come with a diverse skill set and am able to
+          work effectively in both front and back end code.
+        </AboutText>
+        <AboutText>
+          Outside of work I love traveling, gaming, and learning. From traveling
+          to the Philippines, to participating in a{" "}
+          <a
+            style={{ color: ThemeColors.OffWhite }}
+            href="https://ludumdare.com/"
+            target="_blank"
+          >
+            Ludum Dare Game Jam
+          </a>
+          , to learning how to cook and save for retirement, my evenings are
+          often filled with new experiences and adventures.
+        </AboutText>
+        <AboutText>
+          If you are looking for a versatile, adaptable engineer with a desire
+          and drive to produce high-quality, maintainable code, reach out to me
+          today!
+        </AboutText>
       </Box>
     </AboutPage>
   );
@@ -62,10 +87,16 @@ const ProfilePicture = styled(Image)({
   borderColor: ThemeColors.Orange,
 });
 
+const LudumDare = styled("a")({
+  color: ThemeColors.OffWhite,
+});
+
 const AboutText = styled("p")({
   color: ThemeColors.OffWhite,
   fontFamily: "'Maven Pro Variable', sans-serif",
   fontSize: "24px",
 
   marginLeft: "64px",
+
+  userSelect: "none",
 });
