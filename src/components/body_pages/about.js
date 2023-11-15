ca934702ee396a99db45d151cbe428f314dcbf7d
watchis <warren.atchison98@gmail.com>
2023-11-14 21:33:40 -0500

    changing about me section to be better in all ways

--- src/components/body_pages/about.js
+++ src/components/body_pages/about.js
@@ -7,6 +7,11 @@ import { ThemeColors } from "../../consts/colors";
 import { PageTitle } from "../shared/page_title";
 
 const About = () => {
+  const currentDate = new Date();
+
+  const monthMod = currentDate.getMonth() + 1 > 6 ? 1 : 0;
+  const yearsExperience = currentDate.getFullYear() - 2020 + monthMod;
+
   return (
     <AboutPage>
       <Box
@@ -35,29 +40,40 @@ const About = () => {
           <span style={{ color: ThemeColors.Orange }}>About</span> Me
         </PageTitle>
         <AboutText>
-          Hi! I've been a professional software engineer since January 2020 and
-          love to challenge myself to constantly learn new languages, skills,
-          and frameworks.
+          Hi! I'm Warren, a seasoned software engineer with a passion for
+          creating innovative solutions and a track record of success in the
+          dynamic field of technology. With {yearsExperience} years of
+          experience under my belt, I specialize in languages such as Dart, Go,
+          and Java, and have honed my skills working with a diverse range of
+          tools and frameworks including Docker, Kafka, Cassandra, and React.
         </AboutText>
         <AboutText>
-          I pride myself on my ability to adapt to new teams, languages, and
-          frameworks quickly. I am highly self-motivated and get most of my
-          enjoyment at work meeting my own internal goals while advancing
-          project milestones. I come with a diverse skill set and am able to
-          work effectively in both front and back end code.
+          My professional journey includes contributing to the development of
+          enterprise webservices (SaaS) at Workiva and architecting cloud-based
+          microservices with American Express. I thrive on challenges and take
+          pride in my ability to seamlessly integrate into new teams and
+          environments. My diverse skill set allows me to navigate both
+          front-end and back-end code with equal proficiency.
         </AboutText>
         <AboutText>
-          Outside of work I love traveling, gaming, and learning. From traveling
-          to the Philippines, to participating in a{" "}
+          I actively seek opportunities to explore new languages, acquire fresh
+          skills, and master emerging frameworks. Motivated by a desire to
+          achieve personal, team, and project goals, I am driven to deliver
+          high-quality, maintainable code.
+        </AboutText>
+        <AboutText>
+          Beyond the world of coding, I find joy in exploring different cultures
+          and embracing unfamiliar walks of life through travel. Whether I'm
+          developing games in the{" "}
           <a
             style={{ color: ThemeColors.OffWhite }}
             href="https://ludumdare.com/"
             target="_blank"
           >
             Ludum Dare Game Jam
-          </a>
-          , to learning how to cook and save for retirement, my evenings are
-          often filled with new experiences and adventures.
+          </a>{" "}
+          or immersing myself in Path of Exile with friends, my love for gaming
+          is a constant source of inspiration and creativity.
         </AboutText>
         <AboutText>
           If you are looking for a versatile, adaptable engineer with a desire
@@ -96,7 +112,7 @@ const AboutText = styled("p")({
   fontFamily: "'Maven Pro Variable', sans-serif",
   fontSize: "24px",
 
-  marginLeft: "64px",
+  margin: "12px 0 12px 64px",
 
   userSelect: "none",
 });
